71826044e72994eb9037a3224dee03efc52e1ee8
vivek kumar <vivek.kumar@in.nspglobaltech.com>
2025-01-19 20:26:54 +0530

    fix

--- rollup.scss.config.js
+++ rollup.scss.config.js
@@ -10,9 +10,9 @@ export default {
 
   plugins: [
     postcss({
-      extract: path.resolve("dist/styles.css"), // Outputs to dist/styles.css
-      use: ["sass"], // Use Sass for SCSS processing
-      minimize: true, // Minify CSS for production
+      extract: path.resolve("dist/styles.css"), // Outputs to dist/styles.css, itwill help in plain js env if any want our css/scss class name i.e; <link rel="stylesheet" href="path-to-library/dist/styles.css">
+      use: ["sass"], // Configures the plugin to use Sass (node-sass or dart-sass) to process .scss files. & Ensures SCSS files are compiled into standard CSS before being bundled.
+      minimize: true, // Minify CSS for production,removing whitespace, comments, and unnecessary characters.  
     }),
   ],
 };
